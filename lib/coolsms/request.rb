017b81391f98ba97bcaa5296e1374c5fda2859b1
Sungjin Kang <ujuc@ujuc.kr>
2016-07-14 01:34:27 +0900

    Edit request uri
    * sms 부분이 빠져있었음.​

--- lib/coolsms/request.rb
+++ lib/coolsms/request.rb
@@ -1,6 +1,6 @@
 class Request
 
-  #URI
+  # URI
   attr_accessor :uri
 
   def initialize
@@ -8,11 +8,11 @@ class Request
   end
 
   def post(key, fields = {})
-    res = Net::HTTP.post_form(self.uri+"/1.5/#{key}", fields)
+    res = Net::HTTP.post_form(self.uri+"/sms/1.5/#{key}", fields)
   end
 
   def get(key, fields = {})
-    uri = URI(self.uri+"/1.5/#{key}")
+    uri = URI(self.uri+"/sms/1.5/#{key}")
     uri.query = URI.encode_www_form(fields)
     res = Net::HTTP.get_response(uri)
   end
